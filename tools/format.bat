3b4e0fbc647080363a1c8ebd25d1543ff8adb3eb
Marcin Szkudlinski <szkudlik@gmail.com>
2021-12-21 23:55:43 +0100

    Modify format.bat to support polarity

--- tools/format.bat
+++ tools/format.bat
@@ -8,7 +8,7 @@ tools\avrdude.exe -pm328p -cusbasp -Ulfuse:w:0xff:m -Uhfuse:w:0xdf:m -Uefuse:w:0
 tools\avrdude.exe -pm328p -cusbasp -e
 
 ::set chip ID and input polarization in flash
-tools\avrdude.exe -pm328p -cusbasp -Ueeprom:w:%1,%2,%3:m
+tools\avrdude.exe -pm328p -cusbasp -Ueeprom:w:%1,%2,%3,%4:m
 
 ::enable eeprom safe
 tools\avrdude.exe -pm328p -cusbasp -U lfuse:w:0xff:m -U hfuse:w:0xd7:m -U efuse:w:0x04:m
@@ -19,7 +19,7 @@ tools\avrdude.exe -pm328p -cusbasp -Uflash:w:HomeLightControl.ino.eightanaloginp
 goto end
 
 :args_count_wrong
-echo usage: format.bat id_of_node(0x for hex) polarity_inputs0-7  polarity_inputs8-16
+echo usage: format.bat id_of_node(0x for hex) polarity_inputs0-7  polarity_inputs8-16 polarity_outputs0-7 (0 - normal active low, 1 reversed active high priority)
 
 
 :end
